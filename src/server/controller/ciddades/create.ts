0e855e42afbb2beb0d1da10a78434880f0a39570
felixtitonina <felixtitonina@outlook.com>
2024-02-18 18:13:56 -0300

    refactore interface

--- src/server/controller/ciddades/create.ts
+++ src/server/controller/ciddades/create.ts
@@ -2,19 +2,21 @@ import { Request, Response } from 'express';
 import * as yup from 'yup';
 import { validation } from '../../shared/middleware';
 import { StatusCodes } from 'http-status-codes';
+import { ICidade } from '../../database/models';
 
-interface ICidade {
-  nome: string;
-}
+interface IBodyProps extends Omit<ICidade, 'id'> {}
 
 export const createBodyValidation = validation((getSchema) => ({
-  body: getSchema<ICidade>(
+  body: getSchema<IBodyProps>(
     yup.object().shape({
       nome: yup.string().required().min(3),
     }),
   ),
 }));
 
-export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+export const create = async (
+  req: Request<{}, {}, IBodyProps>,
+  res: Response,
+) => {
   return res.status(StatusCodes.CREATED).send('not implemented');
 };
