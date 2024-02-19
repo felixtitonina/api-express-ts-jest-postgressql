a725730c75b125476519e81b7d1b1260b793f654
felixtitonina <felixtitonina@outlook.com>
2024-02-19 14:02:29 -0300

    fix: add seed

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/server';
 import dotenv from 'dotenv';
 dotenv.config();
+const startServer = () => {
+  const port = Number(process.env.PORT) || 3001;
+  console.log('🚀 ~ port:', port);
 
-const port = Number(process.env.PORT) || 3001;
-console.log('🚀 ~ port:', port);
+  server.listen(port, () => console.log(`listening on port ${port}`));
+};
 
-server.listen(port, () => console.log(`listening on port ${port}`));
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate
+    .latest()
+    .then(() => {
+      console.log('🚀 ~ Knex migrate inicializado');
+      Knex.seed
+        .run()
+        .then(() => {
+          console.log('🚀 ~ Knex seed inicializado');
+          startServer();
+        })
+        .catch(console.log);
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
---
+++ src/server/database/seeds/00000_insert_cidades.ts
@@ -0,0 +1,22 @@
+import { Knex } from 'knex';
+import { ETableNames } from '../EtableNames';
+import { cidades } from '../../shared/util/cidades';
+
+export const seed = async (knex: Knex) => {
+  // Deletes ALL existing entries
+  //   await knex(ETableNames.cidade).del();
+
+  const [{ count }] = await knex(ETableNames.cidade).count<[{ count: number }]>(
+    '* as count',
+  );
+
+  if (!Number.isInteger(count) || Number(count) > 0) return;
+
+  const cidadesToInsert = cidades.map((nomeDaCidade) => ({
+    nome: nomeDaCidade,
+  }));
+  console.log('🚀 ~ cidadesToInsert ~ cidadesToInsert:', cidadesToInsert);
+
+  // Inserts seed entries
+  await knex(ETableNames.cidade).insert(cidadesToInsert);
+};
