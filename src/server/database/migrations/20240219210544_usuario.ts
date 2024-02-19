aeb0ba710634fb74fd94fafc0256d101333fdd0d
felixtitonina <felixtitonina@outlook.com>
2024-02-19 18:14:23 -0300

    fix: add migration usuario

---
+++ src/server/database/migrations/20240219210544_usuario.ts
@@ -0,0 +1,23 @@
+import type { Knex } from 'knex';
+import { ETableNames } from '../EtableNames';
+
+export async function up(knex: Knex) {
+  return knex.schema
+    .createTable(ETableNames.usuario, (table) => {
+      table.bigIncrements('id').primary().index();
+      table.string('nome').notNullable().checkLength('>', 3);
+      table.string('senha').notNullable().checkLength('>', 6);
+      table.string('email').index().unique().notNullable().checkLength('>', 5);
+
+      table.comment('Tabela usada para armazenar usuarios do sistema.');
+    })
+    .then(() => {
+      console.log(`# Create table ${ETableNames.usuario}`);
+    });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(ETableNames.usuario).then(() => {
+    console.log(`# Drop table ${ETableNames.usuario}`);
+  });
+}
