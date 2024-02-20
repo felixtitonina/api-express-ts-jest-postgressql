68fcb18e745d9b8b7f28bf2af20cd88bd759fdba
felixtitonina <felixtitonina@outlook.com>
2024-02-20 14:14:25 -0300

    feat: config prod pg

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,5 +1,14 @@
 import { knex } from 'knex';
 import { development, production, test } from './environment';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import pg from 'pg';
+
+if (process.env.NODE_ENV === 'production') {
+  // TODO: pesquisar na document
+  pg.types.setTypeParser(20, 'text', parseInt);
+}
 
 const getEnvironment = () => {
   switch (process.env.NODE_ENV) {
