132a3fa635f5d756882bcd43e4cc8e9186f73672
felixtitonina <felixtitonina@outlook.com>
2024-02-20 16:57:08 -0300

    feat: add cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
-
+import cors from 'cors';
 import './shared/services/translationsYuo';
 import { router } from './routes';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const server = express();
+
+server.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(';') || [],
+  }),
+);
 
 server.use(express.json());
 server.use(router);
